21e541846ae87f525e088155cbc18212d1513bf9
“kangzhehao” <hello@itcast.cn>
2025-06-13 11:29:18 +0800

    init

---
+++ Minity/src/Minity/Application.cpp
@@ -0,0 +1,6 @@
+#include "Application.h"
+#include <iostream>
+
+void ApplicationInit(){
+    std::cout << "Application initialized." << std::endl;
+}
---
+++ SandBox/src/SandBoxApp.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "Application.h"
+
+int main(){
+    std::cout << "Hello, World!" << std::endl;
+    ApplicationInit();
+    return 0;
+}
